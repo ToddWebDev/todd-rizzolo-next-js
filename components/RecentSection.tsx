b3f48d9f9b0a257608e52910d789aa5814b472e8
toddrizzolo <todd.rizzolo@lendio.com>
2023-05-25 11:17:18 -0600

    Update current projects section

--- components/RecentSection.tsx
+++ components/RecentSection.tsx
@@ -4,7 +4,7 @@ import styles from './Components.module.css'
 const Recent = () => (
   <section>
     <h2>My Current Projects</h2>
-    <p>My latest design and development work for Lendio SaaS products.</p>
+    <p>My latest design and development work.</p>
     <div className={styles.cards}>
       <div className={styles.card} style={{ maxWidth: '390px' }}>
         <i aria-hidden className='fas fa-compass'></i>
@@ -22,7 +22,7 @@ const Recent = () => (
         </div>
         <p>
           A &lsquo;from-scratch&rsquo; style guide and component library built with white label
-          theming in mind.
+          theming in mind for Lendio SaaS products.
         </p>
         <ul>
           <li>Vue</li>
@@ -40,6 +40,42 @@ const Recent = () => (
             }
           >
             Visit Guide <i aria-hidden className='fas fa-external-link-alt'></i>
+          </button>
+        </div>
+      </div>
+      <div className={styles.card} style={{ maxWidth: '390px' }}>
+        <i aria-hidden className='fas fa-car'></i>
+        <div style={{ display: 'flex', flexFlow: 'column-reverse' }}>
+          <h3 style={{ marginTop: '-0.75em' }}>Travel Log</h3>
+          <h4
+            style={{
+              fontSize: '0.75em',
+              margin: '0',
+              color: `var(--text-color-medium)`,
+            }}
+          >
+            2023
+          </h4>
+        </div>
+        <p>
+          An interactive travel log built with the MapBox api.
+        </p>
+        <ul>
+          <li>Javascript</li>
+          <li>API</li>
+          <li>UX/UI</li>
+        </ul>
+        <div>
+          <button
+            onClick={() =>
+              window.open(
+                'https://www.toddrizzolo.com/travellog/index.html',
+                '_blank',
+                'noopener'
+              )
+            }
+          >
+            Visit Travel Log <i aria-hidden className='fas fa-external-link-alt'></i>
           </button>
         </div>
       </div>
