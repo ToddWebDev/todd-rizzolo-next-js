713d3ce4fe387fb2e8e8a1d1258bb526df53298b
ToddWebDev <webdevtodd@gmail.com>
2023-06-22 08:03:32 -0600

    Add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,28 @@
+import Script from 'next/script'
 import 'react-circular-progressbar/dist/styles.css'
 import './globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script strategy='afterInteractive' src='https://www.googletagmanager.com/gtag/js?id=G-G35X9H839Z' />
+      <Script
+        id='google-analytics'
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-G35X9H839Z', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
