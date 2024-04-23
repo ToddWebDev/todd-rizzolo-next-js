ed4adda12466c59c2a339b15ecdb42d5bf9b5b07
Todd Rizzolo <todd.rizzolo@adhocteam.us>
2024-04-23 08:42:51 -0600

    Update showcase layout

--- components/HighlightSection.tsx
+++ components/HighlightSection.tsx
@@ -13,20 +13,22 @@ const Highlight = () => {
         A few video highlights of my design and development work for Digital
         Curriculum in 2022.
       </p>
-      <div className={styles.feature}>
-        <div className={styles.video}>
-          <iframe
-            src='https://player.vimeo.com/video/543751841?title=0&autoplay=0&muted=1'
-            title='Digital Curriculum on Ministry Grid'
-            allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
-            width='100%'
-            height='100%'
-          />
+      <div className={styles.features}>
+        <div className={styles.feature}>
+          <div className={styles.video}>
+            <iframe
+              src='https://player.vimeo.com/video/543751841?title=0&autoplay=0&muted=1'
+              title='Digital Curriculum on Ministry Grid'
+              allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+              width='100%'
+              height='100%'
+            />
+          </div>
         </div>
-      </div>
-      <div className={styles.feature} style={{ marginTop: '16px' }}>
-        <div className={styles.video}>
-          <YouTubeEmbed videoId={youtubeVideoId} startTime={startTime} />
+        <div className={styles.feature}>
+          <div className={styles.video}>
+            <YouTubeEmbed videoId={youtubeVideoId} startTime={startTime} />
+          </div>
         </div>
       </div>
     </section>
--- pages/index.tsx
+++ pages/index.tsx
@@ -43,14 +43,37 @@ export default function Home() {
         <link rel='canonical' href='https://www.toddrizzolo.com' />
         <meta name='theme-color' content='#164c69' />
         <meta property='og:type' content='website' />
-        <meta property='og:image' content='https://www.toddrizzolo.com/lendiostyleguide.png'></meta>
-        <meta name='description' content='Todd Rizzolo Software Engineer Portfolio Website' />
+        <meta
+          property='og:image'
+          content='https://www.toddrizzolo.com/lendiostyleguide.png'
+        ></meta>
+        <meta
+          name='description'
+          content='Todd Rizzolo Software Engineer Portfolio Website'
+        />
         <meta name='twitter:card' content='summary_large_image' />
         <meta name='twitter:title' content='Todd Rizzolo • Software Engineer' />
-        <meta name='twitter:description' content='Todd Rizzolo Software Engineer Portfolio Website' />
-        <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
-        <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
-        <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+        <meta
+          name='twitter:description'
+          content='Todd Rizzolo Software Engineer Portfolio Website'
+        />
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/apple-touch-icon.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/favicon-16x16.png'
+        />
       </Head>
 
       <Layout>
@@ -78,7 +101,10 @@ export default function Home() {
           </a>
         </section>
       </Layout>
-      <Script src='https://kit.fontawesome.com/02130b3d51.js' crossOrigin='anonymous' />
+      <Script
+        src='https://kit.fontawesome.com/02130b3d51.js'
+        crossOrigin='anonymous'
+      />
     </>
   )
 }
