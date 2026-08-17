88840b8321370a0ab6d20f24c8cee30c30877eb3
Todd Rizzolo <todd.rizzolo@adhocteam.us>
2026-08-17 08:00:03 -0600

    Remove digital curriculum showcase

--- components/HighlightSection.tsx
+++ components/HighlightSection.tsx
@@ -21,32 +21,6 @@ const Highlight = () => {
           </div>
         </div>
       </div>
-      <p>
-        A few video highlights of my design and development work for Digital
-        Curriculum in 2022.
-      </p>
-      <div className={styles.features}>
-        <div className={styles.feature}>
-          <div className={styles.video}>
-            <iframe
-              src='https://player.vimeo.com/video/543751841?title=0&autoplay=0&muted=1'
-              title='Digital Curriculum on Ministry Grid'
-              allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
-              width='100%'
-              height='100%'
-            />
-          </div>
-        </div>
-        <div className={styles.feature}>
-          <div className={styles.video}>
-            <YouTubeEmbed
-              title="Lifeway's Digital Curriculum"
-              videoId={'ZRaib_BmU_I'}
-              startTime={246}
-            />
-          </div>
-        </div>
-      </div>
     </section>
   )
 }
